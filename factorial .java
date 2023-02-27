893c9a727e53d2809763065e5ff8b19809bfe8fb
miheer <miheerapd@gmail.com>
2023-02-27 16:38:47 +0530

    first commit by atharva SDM Exam

---
+++ factorial .java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = sc.nextInt() ;
+		int ans = 1 ;
+		for(int i =1; i<=num ;i++) {
+			ans = ans * i ;
+		}
+		System.out.println("Factorial of the number"+ num+" is = "+ans);
+		}
+	}
